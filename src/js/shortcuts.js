a5e40a0b045a70a71336d6d148c55220026acaeb
UCyborg <nejc.staric.23@gmail.com>
2024-03-28 01:13:48 +0100

    Convert shortcuts.js to use ES6 arrow functions and consts
    Also changed its style to be like other files.

--- src/js/shortcuts.js
+++ src/js/shortcuts.js
@@ -21,100 +21,111 @@
 
 'use strict';
 
-(function() {
+/******************************************************************************/
 
-    // https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json/commands#Shortcut_values
-    let validStatus0Keys = new Map([
-        [ 'alt', 'Alt' ],
-        [ 'control', 'Ctrl' ],
-    ]);
-    let validStatus1Keys = new Map([
-        [ 'a', 'A' ],
-        [ 'b', 'B' ],
-        [ 'c', 'C' ],
-        [ 'd', 'D' ],
-        [ 'e', 'E' ],
-        [ 'f', 'F' ],
-        [ 'g', 'G' ],
-        [ 'h', 'H' ],
-        [ 'i', 'I' ],
-        [ 'j', 'J' ],
-        [ 'k', 'K' ],
-        [ 'l', 'L' ],
-        [ 'm', 'M' ],
-        [ 'n', 'N' ],
-        [ 'o', 'O' ],
-        [ 'p', 'P' ],
-        [ 'q', 'Q' ],
-        [ 'r', 'R' ],
-        [ 's', 'S' ],
-        [ 't', 'T' ],
-        [ 'u', 'U' ],
-        [ 'v', 'V' ],
-        [ 'w', 'W' ],
-        [ 'x', 'X' ],
-        [ 'y', 'Y' ],
-        [ 'z', 'Z' ],
-        [ '0', '0' ],
-        [ '1', '1' ],
-        [ '2', '2' ],
-        [ '3', '3' ],
-        [ '4', '4' ],
-        [ '5', '5' ],
-        [ '6', '6' ],
-        [ '7', '7' ],
-        [ '8', '8' ],
-        [ '9', '9' ],
-        [ 'f1', 'F1' ],
-        [ 'f2', 'F2' ],
-        [ 'f3', 'F3' ],
-        [ 'f4', 'F4' ],
-        [ 'f5', 'F5' ],
-        [ 'f6', 'F6' ],
-        [ 'f7', 'F7' ],
-        [ 'f8', 'F8' ],
-        [ 'f9', 'F9' ],
-        [ 'f10', 'F10' ],
-        [ 'f11', 'F11' ],
-        [ 'f12', 'F12' ],
-        [ ' ', 'Space' ],
-        [ ',', 'Comma' ],
-        [ '.', 'Period' ],
-        [ 'home', 'Home' ],
-        [ 'end', 'End' ],
-        [ 'pageup', 'PageUp' ],
-        [ 'pagedown', 'PageDown' ],
-        [ 'insert', 'Insert' ],
-        [ 'delete', 'Delete' ],
-        [ 'arrowup', 'Up' ],
-        [ 'arrowdown', 'Down' ],
-        [ 'arrowleft', 'Left' ],
-        [ 'arrowright', 'Right' ],
-        [ 'shift', 'Shift' ],
+(( ) => {
+
+/******************************************************************************/
+
+// https://developer.mozilla.org/en-US/Add-ons/WebExtensions/manifest.json/commands#Shortcut_values
+const validStatus0Keys = new Map([
+    [ 'alt', 'Alt' ],
+    [ 'control', 'Ctrl' ],
+]);
+
+/******************************************************************************/
+
+const validStatus1Keys = new Map([
+    [ 'a', 'A' ],
+    [ 'b', 'B' ],
+    [ 'c', 'C' ],
+    [ 'd', 'D' ],
+    [ 'e', 'E' ],
+    [ 'f', 'F' ],
+    [ 'g', 'G' ],
+    [ 'h', 'H' ],
+    [ 'i', 'I' ],
+    [ 'j', 'J' ],
+    [ 'k', 'K' ],
+    [ 'l', 'L' ],
+    [ 'm', 'M' ],
+    [ 'n', 'N' ],
+    [ 'o', 'O' ],
+    [ 'p', 'P' ],
+    [ 'q', 'Q' ],
+    [ 'r', 'R' ],
+    [ 's', 'S' ],
+    [ 't', 'T' ],
+    [ 'u', 'U' ],
+    [ 'v', 'V' ],
+    [ 'w', 'W' ],
+    [ 'x', 'X' ],
+    [ 'y', 'Y' ],
+    [ 'z', 'Z' ],
+    [ '0', '0' ],
+    [ '1', '1' ],
+    [ '2', '2' ],
+    [ '3', '3' ],
+    [ '4', '4' ],
+    [ '5', '5' ],
+    [ '6', '6' ],
+    [ '7', '7' ],
+    [ '8', '8' ],
+    [ '9', '9' ],
+    [ 'f1', 'F1' ],
+    [ 'f2', 'F2' ],
+    [ 'f3', 'F3' ],
+    [ 'f4', 'F4' ],
+    [ 'f5', 'F5' ],
+    [ 'f6', 'F6' ],
+    [ 'f7', 'F7' ],
+    [ 'f8', 'F8' ],
+    [ 'f9', 'F9' ],
+    [ 'f10', 'F10' ],
+    [ 'f11', 'F11' ],
+    [ 'f12', 'F12' ],
+    [ ' ', 'Space' ],
+    [ ',', 'Comma' ],
+    [ '.', 'Period' ],
+    [ 'home', 'Home' ],
+    [ 'end', 'End' ],
+    [ 'pageup', 'PageUp' ],
+    [ 'pagedown', 'PageDown' ],
+    [ 'insert', 'Insert' ],
+    [ 'delete', 'Delete' ],
+    [ 'arrowup', 'Up' ],
+    [ 'arrowdown', 'Down' ],
+    [ 'arrowleft', 'Left' ],
+    [ 'arrowright', 'Right' ],
+    [ 'shift', 'Shift' ],
     ]);
 
-    let commandNameFromElement = function(elem) {
+/******************************************************************************/
+
+    const commandNameFromElement = elem => {
         while ( elem !== null ) {
-            let name = elem.getAttribute('data-name');
+            const name = elem.getAttribute('data-name');
             if ( typeof name === 'string' && name !== '' ) { return name; }
             elem = elem.parentElement;
         }
     };
 
-    let captureShortcut = function(ev) {
-        let input = ev.target;
-        let name = commandNameFromElement(input);
+/******************************************************************************/
+
+    const captureShortcut = ev => {
+        const input = ev.target;
+        const name = commandNameFromElement(input);
         if ( name === undefined ) { return; }
 
-        let before = input.value;
-        let after = new Set();
+        const before = input.value;
+        const after = new Set();
         let status = 0;
 
-        let updateCapturedShortcut = function() {
+        const updateCapturedShortcut = ( ) => {
             return (input.value = Array.from(after).join('+'));
         };
 
-        let blurHandler = function() {
+        const blurHandler = ( ) => {
             input.removeEventListener('blur', blurHandler, true);
             input.removeEventListener('keydown', keydownHandler, true);
             input.removeEventListener('keyup', keyupHandler, true);
@@ -128,7 +139,7 @@
             }
         };
 
-        let keydownHandler = function(ev) {
+        const keydownHandler = ev => {
             ev.preventDefault();
             ev.stopImmediatePropagation();
             if ( ev.code === 'Escape' ) {
@@ -136,7 +147,7 @@
                 return;
             }
             if ( status === 0 ) {
-                let keyName = validStatus0Keys.get(ev.key.toLowerCase());
+                const keyName = validStatus0Keys.get(ev.key.toLowerCase());
                 if ( keyName !== undefined ) {
                     after.add(keyName);
                     updateCapturedShortcut();
@@ -150,7 +161,7 @@
                     updateCapturedShortcut();
                     return;
                 }
-                let keyName = validStatus1Keys.get(ev.key.toLowerCase());
+                const keyName = validStatus1Keys.get(ev.key.toLowerCase());
                 if ( keyName !== undefined ) {
                     after.add(keyName);
                     updateCapturedShortcut();
@@ -161,11 +172,11 @@
             }
         };
 
-        let keyupHandler = function(ev) {
+        const keyupHandler = ev => {
             ev.preventDefault();
             ev.stopImmediatePropagation();
             if ( status !== 1 ) { return; }
-            let keyName = validStatus0Keys.get(ev.key.toLowerCase());
+            const keyName = validStatus0Keys.get(ev.key.toLowerCase());
             if ( keyName !== undefined && after.has(keyName) ) {
                 after.clear();
                 updateCapturedShortcut();
@@ -185,11 +196,13 @@
         input.addEventListener('keyup', keyupHandler, true);
     };
 
-    let resetShortcut = function(ev) {
-        let name = commandNameFromElement(ev.target);
+/******************************************************************************/
+
+    const resetShortcut = ev => {
+        const name = commandNameFromElement(ev.target);
         if ( name === undefined ) { return; }
 
-        let input = document.querySelector('[data-name="' + name + '"] input');
+        const input = document.querySelector('[data-name="' + name + '"] input');
         if ( input === null ) { return; }
         input.value = '';
         vAPI.messaging.send(
@@ -198,24 +211,26 @@
         );
     };
 
-    let onShortcutsReady = function(commands) {
+/******************************************************************************/
+
+    vAPI.messaging.send('dashboard', { what: 'getShortcuts' }, commands => {
         if ( Array.isArray(commands) === false ) { return; }
-        let template = document.querySelector('#templates .commandEntry');
-        let tbody = document.querySelector('.commandEntries tbody');
-        for ( let command of commands ) {
+        const template = document.querySelector('#templates .commandEntry');
+        const tbody = document.querySelector('.commandEntries tbody');
+        for ( const command of commands ) {
             if ( command.description === '' ) { continue; }
-            let tr = template.cloneNode(true);
+            const tr = template.cloneNode(true);
             tr.setAttribute('data-name', command.name);
             tr.querySelector('.commandDesc').textContent = command.description;
-            let input = tr.querySelector('.commandShortcut input');
+            const input = tr.querySelector('.commandShortcut input');
             input.setAttribute('data-name', command.name);
             input.value = command.shortcut;
             input.addEventListener('focus', captureShortcut);
             tr.querySelector('.commandReset').addEventListener('click', resetShortcut);
             tbody.appendChild(tr);
         }
-    };
-
-    vAPI.messaging.send('dashboard', { what: 'getShortcuts' }, onShortcutsReady);
+    });
 
 })();
+
+/******************************************************************************/
